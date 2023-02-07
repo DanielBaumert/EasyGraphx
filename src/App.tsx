4ebe99c4d1d05be63d242ebb8a0ffebd4b0dcd7d
Daniel Baumert <Daniel.Baumert19@gmail.com>
2023-02-07 21:15:17 +0100

    side-nav

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { drawTextHCenter, mesureText, drawRectangle, drawTextHLeft, Point } from
 import { Field } from './api/Field';
 import { UMLAttribute, UMLAttributeContainer } from './api/UMLAttribute';
 import { UMLClass } from './api/UMLClass';
+import { UMLMethode, UMLMethodeContainer } from './api/UMLMethode';
 
 
 const [store, setStore] = createStore<{ classes: UMLClass[], mouse: Point, readyToMove: boolean }>({
@@ -17,6 +18,15 @@ const [store, setStore] = createStore<{ classes: UMLClass[], mouse: Point, ready
 var exampleClass = new UMLClass();
 exampleClass.isAbstract = true;
 exampleClass.attributes.push(new UMLAttribute());
+exampleClass.attributes.push(new UMLAttribute());
+exampleClass.attributes.push(new UMLAttribute());
+var exampleMethode = new UMLMethode();
+exampleMethode.name = "Abc";
+exampleMethode.returnType = "Integer";
+exampleClass.methodes.push(exampleMethode);
+exampleClass.methodes.push(exampleMethode);
+exampleClass.methodes.push(exampleMethode);
+exampleClass.methodes.push(exampleMethode);
 
 setStore(
   "classes",
@@ -26,7 +36,7 @@ setStore(
   });
 
 const Label: Component<{ title: string }> = (props) =>
-  <label class=" text-sm font-medium text-gray-700">{props.title}</label>
+  <label class=" text-sm font-medium text-gray-700 w-100-full">{props.title}</label>
 
 const Button: Component<{ title: string, onclick: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent> }> = (props) => (
   <button class="
@@ -72,10 +82,15 @@ const App: Component = () => {
 
         var titleSize = mesureText(ctx, umlClass.toString());
         var attrSizes = umlClass.attributes.map(x => mesureText(ctx, x.toString()));
+        var methSizes = umlClass.methodes.map(x => mesureText(ctx, x.toString()));
 
         var maxHeaderBoxSize = titleSize.height;
-        var maxBoxWidth = Math.max(titleSize.width + xPadding, ...attrSizes.map(x => x.width + xPadding));
+        var maxBoxWidth = Math.max(
+          titleSize.width + xPadding,
+          ...methSizes.map(x => x.width + xPadding),
+          ...attrSizes.map(x => x.width + xPadding));
         var maxAttrBoxHeight = attrSizes.reduce((p, c) => p + c.height, 0);
+        var maxMethBoxHeight = methSizes.reduce((p, c) => p + c.height, 0);
 
         drawRectangle(ctx, umlClass.x, umlClass.y, maxBoxWidth, maxHeaderBoxSize, "black", "white");
         drawTextHCenter(ctx, umlClass.x, umlClass.y, maxBoxWidth, xPadding, titleSize, "black");
@@ -87,8 +102,14 @@ const App: Component = () => {
           yOffset += attr.height;
         }
 
+        drawRectangle(ctx, umlClass.x, yOffset, maxBoxWidth, maxMethBoxHeight, "black", "white");
+        for (var meth of methSizes) {
+          drawTextHLeft(ctx, umlClass.x, yOffset, xPadding, meth, "black");
+          yOffset += meth.height;
+        }
+
         umlClass.width = maxBoxWidth;
-        umlClass.height = maxHeaderBoxSize + maxAttrBoxHeight;
+        umlClass.height = maxHeaderBoxSize + maxAttrBoxHeight + maxMethBoxHeight;
 
         ctx.fillStyle = "red";
         ctx.fillRect(umlClass.x, umlClass.y + umlClass.height, umlClass.width, 1);
@@ -111,6 +132,9 @@ const App: Component = () => {
   //   )
   // }
 
+  /*
+   * Attribute management 
+   */
   function pushAttribute() {
     currentClass().attributes.push(new UMLAttribute());
     setCurrentClass(currentClass());
@@ -133,6 +157,36 @@ const App: Component = () => {
     setCurrentClass(currentClass());
     updateView();
   }
+  /*
+   * Methode management
+   */
+  function pushMethode() {
+    currentClass().methodes.push(new UMLMethode());
+    setCurrentClass(currentClass());
+    updateView();
+  }
+
+  function popMethode(methIndex: number) {
+    currentClass().methodes.splice(methIndex, 1);
+    setCurrentClass(currentClass());
+    updateView();
+  }
+
+  function dropMethode(i: number, e: DragEvent) {
+    const src = Number.parseInt(e.dataTransfer.getData("number"));
+    currentClass().methodes.splice(
+      i,
+      0,
+      ...currentClass().methodes.splice(src, 1));
+
+    setCurrentClass(currentClass());
+    updateView();
+  }
+
+
+
+
+
 
   function updateReadyToMove(state: boolean) {
     setStore("readyToMove", (readyToMove) => {
@@ -141,9 +195,9 @@ const App: Component = () => {
     });
   }
 
-  function updateIsStatic(e:Event){ 
-    if(e.currentTarget instanceof HTMLInputElement){ 
-      currentClass().isAbstract = e.currentTarget.checked;  
+  function updateIsStatic(e: Event) {
+    if (e.currentTarget instanceof HTMLInputElement) {
+      currentClass().isAbstract = e.currentTarget.checked;
       updateView();
     }
   }
@@ -200,7 +254,7 @@ const App: Component = () => {
   // }));
 
   return (
-    <div class="relative min-h-screen">
+    <div class="relative min-h-screen max-h-screen">
       <svg class="absolute h-screen w-full">
         <defs>
           <pattern id="bg-image" patternUnits="userSpaceOnUse" width="32" height="32" stroke="#0f172a" stroke-opacity=".2">
@@ -223,25 +277,47 @@ const App: Component = () => {
         }} />
 
       <Show when={currentClass()}>
-        <div id="side-nav" class="fixed top-0 right-0 gap-4 flex flex-col m-4">
-          <div class="bg-white rounded border border-sky-400 px-4 py-2 shadow">
-            <Label title="Class" />
-            <Field title='Name'
-              initValue={currentClass().name}
-              onInputChange={e => { currentClass().name = e.currentTarget.value; updateView() }} />
-            <CheckBox title="Abstract" value={currentClass().isAbstract} onChanges={updateIsStatic} />
-          </div>
-          <div id="attr-container" class="bg-white rounded border border-sky-400 px-2 py-2 shadow">
-            <Label title="Attributes" />
-            <For each={currentClass().attributes}>
-              {(attr, i) => <UMLAttributeContainer
-                index={i()}
-                attr={attr}
-                onDrop={e => dropAttribute(i(), e)}
-                update={updateView}
-                delete={() => popAttribute(i())} />}
-            </For>
-            <Button title='Add attribute' onclick={pushAttribute} />
+        <div id="side-nav" class="fixed flex min-h-screen max-h-screen top-0 right-0 p-4 min-w-[269px]">
+          <div class="flex grow flex-col gap-4 ">
+            <div class="bg-white rounded border border-sky-400 px-4 py-2 shadow">
+              <Label title="Class" />
+              <Field title='Name'
+                initValue={currentClass().name}
+                onInputChange={e => { currentClass().name = e.currentTarget.value; updateView() }} />
+              <CheckBox title="Abstract" value={currentClass().isAbstract} onChanges={updateIsStatic} />
+            </div>
+            <div id="attr-container" class="
+              flex flex-col
+              overflow-hidden max-h-max bg-white rounded border border-sky-400 px-2 py-2 shadow">
+              <Label title="Attributes" />
+              <div class="overflow-y-auto h-full">
+                <For each={currentClass().attributes}>
+                  {(attr, i) => <UMLAttributeContainer
+                    index={i()}
+                    attr={attr}
+                    onDrop={e => dropAttribute(i(), e)}
+                    update={updateView}
+                    delete={() => popAttribute(i())} />}
+                </For>
+              </div>
+              <Button title='Add attribute' onclick={pushAttribute} />
+            </div>
+            <div id="meth-container" class="
+              flex flex-col
+              overflow-hidden max-h-max bg-white rounded border border-sky-400 px-2 py-2 shadow">
+              <Label title="Methodes" />
+              <div class="overflow-y-auto h-full">
+                <For each={currentClass().methodes}>
+                  {(meth, i) => <UMLMethodeContainer
+                    index={i()}
+                    methode={meth}
+                    onDrop={e => dropMethode(i(), e)}
+                    update={updateView}
+                    delete={() => popMethode(i())} />}
+                </For>
+              </div>
+              <Button title='Add methode' onclick={pushMethode} />
+            </div>
           </div>
         </div>
       </Show>
