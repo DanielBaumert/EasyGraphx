ef4422b442502ba02d339772ff6105ab7d6d40fb
Daniel Baumert <Daniel.Baumert19@gmail.com>
2023-02-07 21:17:56 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -287,9 +287,10 @@ const App: Component = () => {
               <CheckBox title="Abstract" value={currentClass().isAbstract} onChanges={updateIsStatic} />
             </div>
             <div id="attr-container" class="
-              flex flex-col
+              flex flex-col h-full
               overflow-hidden max-h-max bg-white rounded border border-sky-400 px-2 py-2 shadow">
               <Label title="Attributes" />
+              <Button title='Add attribute' onclick={pushAttribute} />
               <div class="overflow-y-auto h-full">
                 <For each={currentClass().attributes}>
                   {(attr, i) => <UMLAttributeContainer
@@ -300,12 +301,12 @@ const App: Component = () => {
                     delete={() => popAttribute(i())} />}
                 </For>
               </div>
-              <Button title='Add attribute' onclick={pushAttribute} />
             </div>
             <div id="meth-container" class="
-              flex flex-col
+              flex flex-col h-full
               overflow-hidden max-h-max bg-white rounded border border-sky-400 px-2 py-2 shadow">
               <Label title="Methodes" />
+              <Button title='Add methode' onclick={pushMethode} />
               <div class="overflow-y-auto h-full">
                 <For each={currentClass().methodes}>
                   {(meth, i) => <UMLMethodeContainer
@@ -316,7 +317,6 @@ const App: Component = () => {
                     delete={() => popMethode(i())} />}
                 </For>
               </div>
-              <Button title='Add methode' onclick={pushMethode} />
             </div>
           </div>
         </div>
