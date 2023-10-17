257a654909a98acca3b25e10a8a30de0200676e9
Daniel Baumert <Daniel.Baumert19@gmail.com>
2023-10-17 11:39:38 +0200

    add new type of checkbox (CheckBoxSlim)

--- src/api/CheckBox.tsx
+++ src/api/CheckBox.tsx
@@ -6,7 +6,23 @@ export const CheckBox : Component<{
     value:boolean,
     onChanges:JSX.EventHandlerUnion<HTMLInputElement, Event>}> = (props) => {
     return (
-        <div class="flex items-center mb-2 p-1 rounded hover:shadow">
+        <div class="flex items-center mb-2 p-1 rounded hover:shadow ">
+            <input 
+                id={`checkbox-${props.id}`} 
+                class="w-3.5 h-3.5 text-sky-600 bg-gray-100 border-gray-300 rounded"
+                onChange={props.onChanges} type="checkbox" checked={props.value} />
+            <label for={`checkbox-${props.id}`} class="pl-2 text-sm text-gray-900 w-full">{props.title}</label>
+        </div>);
+}
+
+
+export const CheckBoxSlim : Component<{
+    id: string,
+    title:string, 
+    value:boolean,
+    onChanges:JSX.EventHandlerUnion<HTMLInputElement, Event>}> = (props) => {
+    return (
+        <div class="flex items-center p-1 rounded hover:shadow w-full">
             <input 
                 id={`checkbox-${props.id}`} 
                 class="w-3.5 h-3.5 text-sky-600 bg-gray-100 border-gray-300 rounded"
