2fbb288dab5e7700584cc95decc7d540b431e073
Daniel Baumert <Daniel.Baumert19@gmail.com>
2024-07-30 09:33:49 +0200

    wip

--- src/api/WebGPU/index.tsx
+++ src/api/WebGPU/index.tsx
@@ -6,6 +6,10 @@ export const BLACK: Color = new Float32Array([0.0, 0.0, 0.0, 1.0]);
 export const WHITE: Color = new Float32Array([1.0, 1.0, 1.0, 1.0]);
 
 
+const GPUColors = { 
+  BLACK: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+  WHITE: { r: 1.0, g: 1.0, b: 1.0, a: 1.0 },
+}
 
 export type LineType = {
   0: "solid";
@@ -39,13 +43,56 @@ export type Rectanlge = {
   type: LineType;
 }
 
+const shader_background = `
+const space : f32 = 2.0f / 64.0f;
+const width : f32 = 2.0f / 1920.0f;
+const moveOffset : f32 = 1.0f % space;
 
+struct ScreenInfo { 
+  size: vec2f,
+  offset: vec2f,
+  grid: vec2f, // x => size, y => subdevision
+  color: vec4f
+};
 
+@group(0) @binding(0) var<uniform> screenInfo : ScreenInfo;
 
+@vertex
+fn vs(
+  @builtin(vertex_index) VertexIndex : u32
+) -> @builtin(position) vec4f {
 
+  let offset : f32 = floor(f32(VertexIndex / 6)) * space + moveOffset;
+
+  var pos = array<vec2f, 6>(
+    vec2(-1.0f + offset,        -1),
+    vec2(-1.0f + offset,         1),
+    vec2(-1.0f + offset + width, 1),
+
+    vec2(-1.0f + offset,         -1),
+    vec2(-1.0f + offset + width,  1),
+    vec2(-1.0f + offset + width, -1 + width)
+  );
+
+  return vec4f(pos[VertexIndex % 6], 0.0, 1.0);
+}
+
+@fragment
+fn fs() -> @location(0) vec4f {
+  return vec4(1.0, 0.0, 0.0, 1.0);
+}`
+
+type WebGPUContext = {
+  adapter?: GPUAdapter;
+  device?: GPUDevice;
+  context?: GPUCanvasContext;
+  textureFormat?: GPUTextureFormat;
+
+}
+
+const webGpuContex : WebGPUContext = { }
 
 const isWebGPUSupported = () => navigator.gpu
-
 const getGPUAdapter = async () : Promise<GPUAdapter>  => {
   const adapter = await navigator.gpu.requestAdapter();
   if (!adapter) {
@@ -53,7 +100,6 @@ const getGPUAdapter = async () : Promise<GPUAdapter>  => {
   }
   return adapter;
 }
-
 const getGPUDevice = async (adapter : GPUAdapter) : Promise<GPUDevice> => {
   const device = await adapter.requestDevice();
   if (!device) {
@@ -62,42 +108,10 @@ const getGPUDevice = async (adapter : GPUAdapter) : Promise<GPUDevice> => {
   return device;
 }
 
-
-const createBackgroundShader = (device : GPUDevice) => {
-  const shader = `
-@group(0) @binding(0)
-
-  
-  `;
-
-  const vertices = new Float32Array([
-    -1, -1, // Triangle 1
-     0, -1,
-     0,  0,
-
-    -1, -1, // Triangle 2
-     1,  1,
-    -1,  1,
-  ]);
-
-  const vertexBuffer = device.createBuffer({
-    label: "Cell vertices",
-    size: vertices.byteLength,
-    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-  });
-
-  device.createShaderModule({
-    code: shader,
-  });
-
-  return shader;
-}
-
-
-const configureContext = (context, device) => {
-  context.configure({
-    device: device,
-    format: navigator.gpu.getPreferredCanvasFormat(),
+const configureContext = () => {
+  webGpuContex.context.configure({
+    device: webGpuContex.device,
+    format: webGpuContex.textureFormat = navigator.gpu.getPreferredCanvasFormat(),
     alphaMode: 'premultiplied',
   });
 }
@@ -107,26 +121,91 @@ export const InitWebGpu = async (canvas: HTMLCanvasElement) => {
  if (!isWebGPUSupported()) {
     throw Error("WebGPU not supported.");
   }
+  webGpuContex.adapter = await getGPUAdapter();
+  webGpuContex.device = await getGPUDevice(webGpuContex.adapter);
+  webGpuContex.context = canvas.getContext("webgpu");
 
-  const adapter = await getGPUAdapter();
-  const device = await getGPUDevice(adapter);
-
-
-  const context = canvas.getContext("webgpu");
-
-  configureContext(context, device);
+  configureContext();
   
-  const encoder = device.createCommandEncoder();
-  const pass = encoder.beginRenderPass({
-    colorAttachments: [{
-       view: context.getCurrentTexture().createView(),
-       loadOp: "clear",
-       storeOp: "store",
-       clearValue: { r: 0, g: 0, b: 0.4, a: 1 }, // New line
-    }]
+  const shader = webGpuContex.device.createShaderModule({
+    code: shader_background,
   });
 
-  pass.end();
-  const commandBuffer = encoder.finish();
-  device.queue.submit([commandBuffer]);
+  
+  const pipeline = webGpuContex.device.createRenderPipeline({
+    label: "Background Pipeline",
+    layout: 'auto',
+    vertex: {
+      module: shader,
+      entryPoint: 'vs',
+    },
+    fragment: {
+      module: shader,
+      entryPoint: 'fs',
+      targets: [{ format: webGpuContex.textureFormat }],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    }
+  });
+
+  const screenInfoSize = 
+    2 * 4 + // size
+    2 * 4 + // offset
+    2 * 4 + // grid
+    4 * 4;  // color
+
+  const screenInfo = webGpuContex.device.createBuffer({
+    label: "Screen Info",
+    size: screenInfoSize,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+
+  const aspect = canvas.width / canvas.height;
+
+  const uniformValues = new Float32Array(screenInfoSize / 4);
+  uniformValues.set([canvas.width / aspect, canvas.height], 0);
+  uniformValues.set([0, 0], 2);
+  uniformValues.set([64, 64], 4);
+  uniformValues.set([1, 0, 0, 1], 6);
+
+  const bindGroup = webGpuContex.device.createBindGroup({
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: {
+        buffer: screenInfo
+      }
+    }]
+  })
+
+  const frame = () => { 
+
+    const commandEncoder = webGpuContex.device.createCommandEncoder();
+    const textureView = webGpuContex.context.getCurrentTexture().createView();
+
+    webGpuContex.device.queue.writeBuffer(screenInfo, 0, uniformValues);
+
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          clearValue: [0.0, 0.0, 0.0, 1.0],
+          loadOp: 'clear',
+          storeOp: 'store',
+        },
+      ],
+    };
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.setBindGroup(0, bindGroup);
+    passEncoder.draw(6 * 65);
+    passEncoder.end();
+
+    webGpuContex.device.queue.submit([commandEncoder.finish()]);
+   /// requestAnimationFrame(frame);
+  }
+
+  requestAnimationFrame(frame);
 }
